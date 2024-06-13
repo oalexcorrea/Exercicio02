292cbaa5d547c18daed8618323c09e61e4cd606c
Alex Correa <144955928+oalexcorrea@users.noreply.github.com>
2024-06-13 14:55:43 -0300

    Add files via upload

---
+++ Exercicio02/scripts.js
@@ -0,0 +1,58 @@
+const students = [
+  {
+    name: "Sarah",
+    grade1: 7,
+    grade2: 8,
+  },
+  {
+    name: "Alex",
+    grade1: 0,
+    grade2: 5,
+  },
+  {
+    name: "Carlos",
+    grade1: 10,
+    grade2: 10,
+  },
+  {
+    name: "Beth",
+    grade1: 9,
+    grade2: 9,
+  }
+]
+
+function calculateGrade() {
+  for(let i = 0; i < students.length; i++) {
+    let student = students[i].name
+    let finalGrade = (students[i].grade1 + students[i].grade2) / 2
+  
+
+  if(finalGrade < 7) {
+    alert(`A média do(a) aluno(a) ${student} é: ${finalGrade}
+    Não foi dessa vez ${student}! Tente novamente!`)
+  } else {
+    alert(`A média do(a) aluno(a) ${student} é: ${finalGrade}
+    Parabéns ${student}! Você foi aprovado(a) no concurso!`)
+  }
+}
+}
+
+
+calculateGrade()
+
+
+
+
+
+
+
+/*
+function liquidificador(fruta1, fruta2) {
+  const suco = fruta1 + fruta2
+  return suco
+}
+
+let meuSuco = liquidificador(2, "banana")
+
+alert(meuSuco)
+*/
